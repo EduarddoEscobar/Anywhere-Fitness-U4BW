575865ddfc901a47fb84cbf2f98c77e35c84c964
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-03-04 13:45:38 -0500

    added some structure to make all the routers

--- api/data/db-config.js
+++ api/data/db-config.js
@@ -1,4 +1,5 @@
 const knex = require('knex')
 const configs = require('../../knexfile')
+const environment = process.env.NODE_ENV || 'development'
 
-module.exports = knex(configs[process.env.NODE_ENV])
+module.exports = knex(configs[environment]);
---
+++ api/endpoints/users/users-model.js
@@ -0,0 +1,47 @@
+const db = require('../../data/db-config');
+
+function getAll() {
+    return db('users as u')
+        .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+        .select('u.user_id', 'u.username', 'r.role_name');
+}
+
+function getById(id) {
+    return db('users as u')
+        .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+        .select('u.user_id', 'u.username', 'r.role_name')
+        .where('u.user_id', id)
+        .first();
+}
+
+function getByRole(role) {
+    return db('users as u')
+        .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+        .select('u.user_id', 'u.username', 'r.role_name')
+        .where('r.role_name', role)
+}
+
+async function add(user) {
+    let [id] = await db('users').insert(user);
+    return getById(id);
+}
+
+async function update(id, changes) {
+    await db('users').update(changes).where('user_id', id);
+    return getById(id);
+}
+
+async function remove(id) {
+    let user = await findById(id);
+    await db('users').where('user_id', id).del();
+    return user;
+}
+
+module.exports = {
+    getAll,
+    getById,
+    getByRole,
+    add,
+    update,
+    remove
+}
